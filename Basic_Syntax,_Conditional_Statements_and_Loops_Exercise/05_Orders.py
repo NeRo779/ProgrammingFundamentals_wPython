d6bde5094e3a3c45bcf30a26aadc992aa786a1d2
Christian Boshev <135061477+NeRo779@users.noreply.github.com>
2024-08-08 19:24:00 +0300

    Create 05_Orders.py

---
+++ Basic_Syntax,_Conditional_Statements_and_Loops_Exercise/05_Orders.py
@@ -0,0 +1,28 @@
+number_of_orders = int(input())
+coffee_price = 0
+total_price = 0
+
+for order in range(number_of_orders):
+    price_per_capsule = float(input())
+    days = int(input())
+    capsules_per_day = int(input())
+
+    # if not 0.01 <= price_per_capsule <= 100.00:
+    #     continue
+    # days = int(input())
+    # if not 1 <= days <= 31:
+    #     continue
+    # capsules_per_day = int(input())
+    # if not 1 <= capsules_per_day <= 2000:
+    #     continue
+
+    if not 0.01 <= price_per_capsule <= 100.00 \
+            or not 1 <= days <= 31 \
+            or not 1 <= capsules_per_day <= 2000:
+        continue
+
+    coffee_price = price_per_capsule * days * capsules_per_day
+    print(f'The price for the coffee is: ${coffee_price:.2f}')
+    total_price += coffee_price
+
+print(f'Total: ${total_price:.2f}')
