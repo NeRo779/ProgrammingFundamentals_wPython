e29042e92f64ec1a1c3cdfd64c7b99152f4388bd
Christian Boshev <135061477+NeRo779@users.noreply.github.com>
2024-08-11 16:10:09 +0300

    Update 01_Smallest_Of_Three_Numbers.py

--- Functions_Exercise/01_Smallest_Of_Three_Numbers.py
+++ Functions_Exercise/01_Smallest_Of_Three_Numbers.py
@@ -1,1 +1,10 @@
+def smallest(some_nums: list):
+    return min(some_nums)
+
+first = int(input())
+second = int(input())
+third = int(input())
+
+smallest_element = smallest([first, second, third])
+print(smallest_element)
 
