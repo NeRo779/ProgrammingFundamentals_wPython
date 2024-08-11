8c8a3f807039891e687d7a9ba8ad2a0e3083e381
Christian Boshev <135061477+NeRo779@users.noreply.github.com>
2024-08-11 16:03:20 +0300

    Update 01_Invert_Values.py

--- Lists_Basics_Exercise/01_Invert_Values.py
+++ Lists_Basics_Exercise/01_Invert_Values.py
@@ -1,1 +1,9 @@
+numbers_list = input().split()
+opposite_number = []
+
+for number in numbers_list:
+    current_number = -int(number)
+    opposite_number.append(current_number)
+print(opposite_number)
+
 
