3d1bcd2353ba2c501cf782bad51e5181f53b5eb1
Christian Boshev <135061477+NeRo779@users.noreply.github.com>
2024-08-12 12:44:32 +0300

    Create 07_Min_Max_and_Sum.py

---
+++ Functions_Exercise/07_Min_Max_and_Sum.py
@@ -0,0 +1,7 @@
+numbers = input().split()
+int_numbers = [int(x) for x in numbers]
+
+print(f"The minimum number is {min(int_numbers)}")
+print(f"The maximum number is {max(int_numbers)}")
+print(f"The sum number is: {(sum(int_numbers))}")
+
