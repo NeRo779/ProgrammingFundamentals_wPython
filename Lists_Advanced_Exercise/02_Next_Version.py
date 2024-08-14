1727d92bf894a43b217dd5c03806bff098bdaa42
Christian Boshev <135061477+NeRo779@users.noreply.github.com>
2024-08-14 16:15:40 +0300

    Create 02_Next_Version.py

---
+++ Lists_Advanced_Exercise/02_Next_Version.py
@@ -0,0 +1,9 @@
+current_version = [int(digit) for digit in input().split(".")]
+current_version [-1] += 1
+for index in range(len(current_version) - 1, 0, -1):
+    if current_version[index] > 9:
+        current_version[index] = 0
+        current_version [index - 1] += 1
+
+print(".".join(str(index) for index in current_version))
+
