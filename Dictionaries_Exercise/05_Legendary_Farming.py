3dd2f53f8c2c4ab77c256d8daf0a4dac91b28a08
Christian Boshev <135061477+NeRo779@users.noreply.github.com>
2024-09-05 16:37:42 +0300

    Create 05_Legendary_Farming.py

---
+++ Dictionaries_Exercise/05_Legendary_Farming.py
@@ -0,0 +1,29 @@
+items = {"shards": 0, "fragments": 0, "motes": 0}
+obtained = False
+
+while not obtained:
+    item = input().split()
+    for index in range(0, len(item), 2):
+        value = int(item[index])
+        key = item[index + 1].lower()
+        if key not in items.keys():
+            items[key] = 0
+        items[key] += value
+        if items["shards"] >= 250:
+            print("Shadowmourne obtained!")
+            items["shards"] -= 250
+            obtained = True
+        elif items["fragments"] >= 250:
+            print("Valanyr obtained!")
+            items["fragments"] -= 250
+            obtained = True
+        elif items["motes"] >= 250:
+            print("Dragonwrath obtained!")
+            items["motes"] -= 250
+            obtained = True
+        if obtained:
+            break
+
+for material, quantity in items.items():
+    print(f"{material}: {quantity}")
+
