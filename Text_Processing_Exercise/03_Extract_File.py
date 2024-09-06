da8d542816443430bda258a3f8f2019e7075c76d
Christian Boshev <135061477+NeRo779@users.noreply.github.com>
2024-09-06 20:32:19 +0300

    Create 03_Extract_File.py

---
+++ Text_Processing_Exercise/03_Extract_File.py
@@ -0,0 +1,6 @@
+path_file = input().split("\\")
+file_name_and_extension = path_file[-1]
+file_name, file_extension = file_name_and_extension.split(".")
+print(f"File name: {file_name}")
+print(f"File extension: {file_extension}")
+
